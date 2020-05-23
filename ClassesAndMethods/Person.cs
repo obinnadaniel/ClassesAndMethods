19384592ab2db4eb5b77c54a2f3db4ad2fc52bc8
Obinna Daniel <obinnadaniel30@yahoo.com>
2020-05-23 05:25:13 +0100

    initial commit

---
+++ ClassesAndMethods/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public bool HasPet;
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hi! My name is " + Name + " and my age is " + Age);
+        }
+    }
+}
---
+++ ClassesAndMethods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Daniel";
+            person.Age = 32;
+            person.HasPet = true;
+
+            person.Greeting();
+            Console.ReadLine();
+        }
+    }
+}
